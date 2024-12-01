afc43a3b13eed03eaa73b6ab94d6b6db5d9d8b9c
Happy <sk24coc@gmail.com>
2024-12-01 11:22:49 +0530

    Day 1 done

---
+++ Day01/Day01_sol_1.py
@@ -0,0 +1,21 @@
+input_file = "Day01_input.txt"
+input_data = []
+
+left = []
+right = []
+
+with open(input_file, "r") as f:
+    input_data = f.readlines()
+    input_data = [(left.append(int(line.strip().split()[0])), 
+                   right.append(int(line.strip().split()[1]))) 
+                        for line in input_data]
+
+left.sort()
+right.sort()
+
+assert(len(left) == len(right))
+
+summ = 0
+for l, r in zip(left, right):
+    summ += abs(l-r)
+print(summ)
---
+++ Day01/Day01_sol_2.py
@@ -0,0 +1,25 @@
+input_file = "Day01_input.txt"
+input_data = []
+
+left = []
+right = []
+
+with open(input_file, "r") as f:
+    input_data = f.readlines()
+    input_data = [(left.append(int(line.strip().split()[0])), 
+                   right.append(int(line.strip().split()[1]))) 
+                        for line in input_data]
+
+dictt = {}
+for i in right:
+    if i in dictt:
+        dictt[i] += 1
+    else :
+        dictt[i] = 1
+summ = 0
+for i in left:
+    if i in dictt:
+        summ += i*dictt[i]
+    else:
+        summ += 0
+print(summ)
